8112783f6f8a15997144f7357b2cc86d2941b5da
ReihanFatilla <reihanfatilla@gmail.com>
2022-06-15 14:48:15 +0800

    [feat]: added ApiService.kt and MovieResponse.kt

--- app/src/main/java/com/reift/movieapp/data/remote/ApiService.kt
+++ app/src/main/java/com/reift/movieapp/data/remote/ApiService.kt
@@ -1,5 +1,46 @@
 package com.reift.movieapp.data.remote
 
-class ApiService {
-    
+import com.reift.movieapp.data.MovieResponse
+import io.reactivex.rxjava3.core.Flowable
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface ApiService {
+    @GET("movie/{type}")
+    fun getMovieList(
+        @Path("type")
+        type: String,
+        @Query("api_key")
+        apiKey: String,
+        @Query("region")
+        region: String,
+        @Query("page")
+        page: String = "1"
+    ): Flowable<MovieResponse>
+
+    @GET("movie/{id}/recommendations")
+    fun getMovieRecommendationsById(
+        @Path("id")
+        id: String,
+        @Query("api_key")
+        apiKey: String,
+        @Query("region")
+        region: String,
+        @Query("page")
+        page: String = "1"
+    ): Flowable<MovieResponse>
+
+    @GET("discover/movie")
+    fun getMovieSortBy(
+        @Query("api_key")
+        apiKey: String,
+        @Query("sort_by")
+        sortBy: String,
+        @Query("region")
+        region: String,
+        @Query("page")
+        page: String = "1"
+    ): Flowable<MovieResponse>
+
 }
