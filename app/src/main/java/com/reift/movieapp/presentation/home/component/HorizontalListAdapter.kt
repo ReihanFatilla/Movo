a962154084c34a32022264b8b54097a0edf7d5e4
ReihanFatilla <reihanfatilla@gmail.com>
2022-07-03 20:41:08 +0800

    [fixed]: finished HorizontalListAdapter.kt

--- app/src/main/java/com/reift/movieapp/presentation/home/component/HorizontalListAdapter.kt
+++ app/src/main/java/com/reift/movieapp/presentation/home/component/HorizontalListAdapter.kt
@@ -3,11 +3,23 @@ package com.reift.movieapp.presentation.home.component
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.Priority
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.reift.movieapp.HelperFunction
+import com.reift.movieapp.constant.Constant
 import com.reift.movieapp.data.ResultsItem
 import com.reift.movieapp.databinding.ItemHorizontalMovieBinding
 
 class HorizontalListAdapter: RecyclerView.Adapter<HorizontalListAdapter.MyViewHolder>() {
     val listMovie = ArrayList<ResultsItem>()
+
+    fun setData(data: List<ResultsItem>?) {
+        if (data == null) return
+        listMovie.clear()
+        listMovie.addAll(data)
+    }
 
     class MyViewHolder(val binding: ItemHorizontalMovieBinding): RecyclerView.ViewHolder(binding.root)
 
@@ -16,14 +28,24 @@ class HorizontalListAdapter: RecyclerView.Adapter<HorizontalListAdapter.MyViewHo
     )
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val ratingCount = "(${listMovie[position].voteAverage})"
-
+        val ratingCount = listMovie[position].voteAverage
+        val ratingString = "($ratingCount)"
         holder.binding.apply {
-            tvTitle.text = listMovie[position].title
+            Glide.with(imgMovie.context)
+                .load(Constant.IMAGE_BASE_URL+listMovie[position].posterPath)
+                .apply(RequestOptions())
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .priority(Priority.HIGH)
+                .into(imgMovie)
+            tvTitle.text = listMovie[position].title ?: listMovie[position].originalName
+            tvRating.text = ratingString
+            HelperFunction.setUpRatingStars(
+                holder.itemView.context,
+                holder.binding,
+                ratingCount!!
+            )
         }
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount() = listMovie.size
 }
