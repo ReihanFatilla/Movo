36087122e9a40f8d032d4fa9cd4e5f5c109b278f
ReihanFatilla <reihanfatilla@gmail.com>
2022-07-04 21:22:57 +0800

    [feat]: added credit adapter

---
+++ app/src/main/java/com/reift/movieapp/presentation/detail/component/CreditAdapter.kt
@@ -0,0 +1,44 @@
+package com.reift.movieapp.presentation.detail.component
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.Priority
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.reift.movieapp.constant.Constant
+import com.reift.movieapp.data.response.CastItem
+import com.reift.movieapp.data.response.CreditResponse
+import com.reift.movieapp.databinding.ItemCreditDetailBinding
+
+class CreditAdapter: RecyclerView.Adapter<CreditAdapter.MyViewHolder>()  {
+    private val listCredit = ArrayList<CastItem>()
+
+    class MyViewHolder(val binding: ItemCreditDetailBinding): RecyclerView.ViewHolder(binding.root)
+
+    fun setData(list: List<CastItem>?){
+        if (list == null) return
+        listCredit.clear()
+        listCredit.addAll(list)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)= MyViewHolder(
+        ItemCreditDetailBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+    )
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.binding.apply {
+            tvName.text = listCredit[position].name
+            tvCharacter.text = listCredit[position].character
+            Glide.with(imgProfileCredit.context)
+                .load(Constant.IMAGE_BASE_URL+listCredit[position].profilePath)
+                .apply(RequestOptions())
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .priority(Priority.HIGH)
+                .into(imgProfileCredit)
+        }
+    }
+
+    override fun getItemCount() = listCredit.size
+}
