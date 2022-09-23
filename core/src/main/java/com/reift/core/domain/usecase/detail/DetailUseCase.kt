9b1d69e690e57565e297c2f7f302f19bb8c1b27b
ReihanFatilla <reihanfatilla@gmail.com>
2022-09-23 13:28:32 +0700

    [feat]: finished detail usecase

--- core/src/main/java/com/reift/core/domain/usecase/detail/DetailUseCase.kt
+++ core/src/main/java/com/reift/core/domain/usecase/detail/DetailUseCase.kt
@@ -1,4 +1,14 @@
 package com.reift.core.domain.usecase.detail
 
+import com.reift.core.domain.model.Resource
+import com.reift.core.domain.model.detail.*
+import io.reactivex.rxjava3.core.Flowable
+
 interface DetailUseCase {
+    fun getMovieDetail(id: String): Flowable<Resource<MovieDetail>>
+    fun getTvDetail(id: String): Flowable<Resource<TvDetail>>
+
+    fun getReviews(id: String): Flowable<Resource<Review>>
+    fun getWallpapers(id: String): Flowable<Resource<Wallpaper>>
+    fun getActors(id: String): Flowable<Resource<Actor>>
 }
