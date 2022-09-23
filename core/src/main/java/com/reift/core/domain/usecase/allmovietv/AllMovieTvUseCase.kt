a0ee03335ffdc360ae7c251803bf00d1382f6107
ReihanFatilla <reihanfatilla@gmail.com>
2022-09-23 13:31:26 +0700

    [feat]: finished allmovie usecase

--- core/src/main/java/com/reift/core/domain/usecase/allmovietv/AllMovieTvUseCase.kt
+++ core/src/main/java/com/reift/core/domain/usecase/allmovietv/AllMovieTvUseCase.kt
@@ -1,4 +1,11 @@
 package com.reift.core.domain.usecase.allmovietv
 
+import com.reift.core.domain.model.Resource
+import com.reift.core.domain.model.movie.MovieResult
+import com.reift.core.domain.model.tv.TvResult
+import io.reactivex.rxjava3.core.Flowable
+
 interface AllMovieTvUseCase {
+    fun getMovies(category: String, page: String): Flowable<Resource<MovieResult>>
+    fun getTvShow(category: String, page: String): Flowable<Resource<TvResult>>
 }
