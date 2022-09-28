ca396177733bcb255e8e0d1cd5d342eead7e6d48
ReihanFatilla <reihanfatilla@gmail.com>
2022-09-28 13:04:27 +0700

    [feat]: added NetworkModule.kt

---
+++ core/src/main/java/com/reift/core/di/NetworkModule.kt
@@ -0,0 +1,40 @@
+package com.reift.core.di
+
+import com.reift.core.BuildConfig
+import com.reift.core.data.remote.source.network.ApiService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+val networkModule = module {
+    single {
+        OkHttpClient.Builder()
+            .retryOnConnectionFailure(true)
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .addInterceptor {
+                val request = it.request()
+                    .newBuilder()
+                    .addHeader("Content-Type", "application/json")
+                    .build()
+                return@addInterceptor it.proceed(request)
+            }
+            .pingInterval(10, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .build()
+    }
+
+    single {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+            .client(get())
+            .build()
+        retrofit.create(ApiService::class.java)
+    }
+}
