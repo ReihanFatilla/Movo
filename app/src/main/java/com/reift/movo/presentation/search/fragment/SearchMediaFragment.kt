fcb4b0b56b11d2b464998d7f1062e3b96fc99d90
ReihanFatilla <reihanfatilla@gmail.com>
2022-11-18 14:45:57 +0700

    [feat]: set up binding on search media fragment

--- app/src/main/java/com/reift/movo/presentation/search/fragment/SearchMediaFragment.kt
+++ app/src/main/java/com/reift/movo/presentation/search/fragment/SearchMediaFragment.kt
@@ -6,14 +6,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.reift.movo.R
+import com.reift.movo.databinding.FragmentSearchMediaBinding
+import com.reift.movo.databinding.FragmentSearchTabBinding
 
 class SearchMediaFragment : Fragment() {
+
+    private var _binding: FragmentSearchMediaBinding? = null
+    private val binding get() = _binding as FragmentSearchMediaBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search_media, container, false)
+    ): View {
+
+        _binding = FragmentSearchMediaBinding.inflate(layoutInflater)
+
+        return binding.root
     }
 
 }
